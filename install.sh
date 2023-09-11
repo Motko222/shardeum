1289d6bfa39ce91d13a76c52b66db3f81a61b8fa
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-11 22:53:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,12 @@ sudo chmod +x /usr/local/bin/docker-compose
 curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
 chmod +x installer.sh
 ./installer.sh;
+
+FILE=~/config/shardeum.sh
+if [ -f "$FILE" ]; then
+    echo "$FILE exists, skipping creation..."
+else 
+    echo "$FILE does not exist, creating..."
+    echo "#!/bin/bash" > $FILE
+    echo "id=optional" >> $FILE
+fi
