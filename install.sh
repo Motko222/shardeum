53f6dccb777c4d6aac9e49e391953da509a44fac
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-11 13:50:04 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+apt upgrade -y
+sudo apt-get install curl jq
+sudo apt install docker.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
+chmod +x installer.sh
+./installer.sh;
