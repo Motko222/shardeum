891eba6a4ee5ec19fae923392ed2e67153504f75
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-12 16:19:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-apt upgrade -y
-sudo apt-get install curl jq
-sudo apt install docker.io
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
-chmod +x installer.sh
-./installer.sh;
-
 FILE=~/config/shardeum.sh
 if [ -f "$FILE" ]; then
     echo "$FILE exists, skipping creation..."
@@ -18,3 +8,13 @@ else
     echo '#!/bin/bash' > $FILE
     echo 'id=optional' >> $FILE
 fi
+
+sudo apt update
+apt upgrade -y
+sudo apt-get install curl jq
+sudo apt install docker.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
+chmod +x installer.sh
+./installer.sh
