9623ff246f856ea4aa4a93d30c39606b463fedda
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-11-09 09:04:14 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -6,9 +6,9 @@ note=""
 port=$(cat ~/.shardeum/.env | grep SHMEXT | cut -d "=" -f 2)
 sts=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.status | sed 's/"//g')
 case $sts in
- null) status="ok";note="standby" ;;
+ null) status="ok";note="standby ($sts)" ;;
  active) status="ok";note="active" ;;
- *) status="error";note="API error" ;;
+ *) status="error";note="API error ($sts)" ;;
 esac
 version=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.appData.shardeumVersion | sed 's/\"//g')
 network="sphinx"
