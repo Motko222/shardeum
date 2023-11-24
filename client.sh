e031674359c1b890cfc7807b51ccc85e10e83a99
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-11-24 10:43:56 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Shell commands:"
-echo "Start node:    operator-cli gui start; operator-cli start; exit"
-echo "Status:        operator-cli gui status; operator-cli status;"
+echo "Start node:        operator-cli gui start; operator-cli start; exit"
+echo "Status:            operator-cli gui status; operator-cli status;"
 cd ~/.shardeum
 ./shell.sh
