4a24d3bc14af095a32e679537265844520e0bf39
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-12-05 09:49:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ if [ -d ~/.shardeum ]
 then
  read -p "Shardeum node already installed, this action will purge and re-install. Please unstake your tokens. Continue? " sure
  case $sure in
-  y,Y)
+  y|Y)
    echo "Stopping running cointainer..."
-   # bash ~/scripts/shardeum/stop.sh
+   bash ~/scripts/shardeum/stop.sh
    echo "Removing directory..."
-   # rm -r ~/.shardeum
+   rm -r ~/.shardeum
   ;;
   *)
    exit=true
@@ -30,13 +30,13 @@ else
       cp ~/scripts/shardeum/config/env.sample ~/scripts/shardeum/config/env
   fi
   echo "Installing..."
-#  sudo apt update
-#  apt upgrade -y
-#  sudo apt-get install curl jq
-#  sudo apt install docker.io
-#  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#  sudo chmod +x /usr/local/bin/docker-compose
-#  curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
-#  chmod +x installer.sh
-#  ./installer.sh
+  sudo apt update
+  apt upgrade -y
+  sudo apt-get install curl jq
+  sudo apt install docker.io
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
+  chmod +x installer.sh
+  ./installer.sh
 fi
