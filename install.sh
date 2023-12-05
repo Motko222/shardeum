d9990a748ee6e4ec00354311a0b5c1260b7dc928
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-12-05 09:14:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-cp ~/scripts/shardeum/config/env.sample ~/scripts/shardeum/config/env
+if [ -d ~/.shardeum]
+then
+ read -p "Shardeum node already installed, this action will purge and re-install. Are you sure? " sure
+ case $sure in
+  y,Y)
+   echo "Stopping running cointainer..."
+   
+   echo "Removing directory..."
+   rm -r ~/.shardeum
+  ;;
+  *)
+   exit=true
+  ;;
+  esac
+ else
+  echo "Sharedeum node installation not found, not installing..."
+ fi
 
+if [ $exit ]
+then
+ echo "Installation cancelled."
+else
   if [ -f ~/scripts/shardeum/config/env ] 
     then
       echo "Config file found."
@@ -9,13 +29,13 @@ cp ~/scripts/shardeum/config/env.sample ~/scripts/shardeum/config/env
       echo "Config file not found, creating one."
       cp ~/scripts/shardeum/config/env.sample ~/scripts/shardeum/config/env
   fi
-
-sudo apt update
-apt upgrade -y
-sudo apt-get install curl jq
-sudo apt install docker.io
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
-chmod +x installer.sh
-./installer.sh
+ sudo apt update
+ apt upgrade -y
+ sudo apt-get install curl jq
+ sudo apt install docker.io
+ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ sudo chmod +x /usr/local/bin/docker-compose
+ curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
+ chmod +x installer.sh
+ ./installer.sh
+fi
