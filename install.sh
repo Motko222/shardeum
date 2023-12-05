8da898c818bee3f029c6b279c612503810d00ca3
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-12-05 09:15:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ then
  case $sure in
   y,Y)
    echo "Stopping running cointainer..."
-   
+   bash ~/scripts/shardeum/stop.sh
    echo "Removing directory..."
    rm -r ~/.shardeum
   ;;
@@ -29,13 +29,13 @@ else
       echo "Config file not found, creating one."
       cp ~/scripts/shardeum/config/env.sample ~/scripts/shardeum/config/env
   fi
- sudo apt update
- apt upgrade -y
- sudo apt-get install curl jq
- sudo apt install docker.io
- sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
- sudo chmod +x /usr/local/bin/docker-compose
- curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
- chmod +x installer.sh
- ./installer.sh
+  sudo apt update
+  apt upgrade -y
+  sudo apt-get install curl jq
+  sudo apt install docker.io
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
+  chmod +x installer.sh
+  ./installer.sh
 fi
