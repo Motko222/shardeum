6d8bccbc7a2ccf5d788eb1f38129241f2ce980fa
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-12-05 09:28:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ then
   ;;
   esac
  else
-  echo "Sharedeum node installation not found, not installing..."
+  echo "Sharedeum node installation not found."
  fi
 
 if [ $exit ]
@@ -29,13 +29,14 @@ else
       echo "Config file not found, creating one."
       cp ~/scripts/shardeum/config/env.sample ~/scripts/shardeum/config/env
   fi
-  sudo apt update
-  apt upgrade -y
-  sudo apt-get install curl jq
-  sudo apt install docker.io
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-  curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
-  chmod +x installer.sh
-  ./installer.sh
+  echo "Installing..."
+#  sudo apt update
+#  apt upgrade -y
+#  sudo apt-get install curl jq
+#  sudo apt install docker.io
+#  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#  sudo chmod +x /usr/local/bin/docker-compose
+#  curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
+#  chmod +x installer.sh
+#  ./installer.sh
 fi
