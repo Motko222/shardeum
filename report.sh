7149ce9d1dbcfee06cd00b07b8f0cdfdae9b0e9f
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-28 10:20:55 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -10,8 +10,8 @@
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 docker_status=$(docker inspect shardeum-dashboard | jq -r .[].State.Status)
 folder_size=$(du -hs $HOME/.shardeum | awk '{print $1}')
-version=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.appData.shardeumVersion | sed 's/\"//g')
 port=$(cat ~/.shardeum/.env | grep SHMEXT | cut -d "=" -f 2)
+version=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.appData.shardeumVersion | sed 's/\"//g')
 node_status=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.status | sed 's/"//g')
 
 case $node_status in
@@ -34,7 +34,6 @@ cat << EOF
   "type":"node",
   "status":"$status",
   "note":"$note",
-  "docker":"$docker_status",
   "folder_size":"$folder_size",
   "updated":"$(date --utc +%FT%TZ)",
   "docker_status":"$docker_status",
