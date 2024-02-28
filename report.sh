1999110ecf59de0e0209cee49cbd531b00b5227a
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-28 10:18:04 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
-source ~/scripts/shardeum/config/env
+#docker compose safe
+#if command -v docker-compose &>/dev/null
+#then docker_compose="docker-compose"
+#elif docker --help | grep -q "compose"
+#then docker_compose="docker compose"
+#fi
 
-note=""
-port=$(cat ~/.shardeum/.env | grep SHMEXT | cut -d "=" -f 2)
-sts=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.status | sed 's/"//g')
-case $sts in
- null) status="ok";note="standby ($sts)" ;;
- active) status="ok";note="active" ;;
- *) status="error";note="API error ($sts)" ;;
-esac
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+docker_status=$(docker inspect shardeum-dashboard | jq -r .[].State.Status)
+folder_size=$(du -hs $HOME/.shardeum | awk '{print $1}')
 version=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.appData.shardeumVersion | sed 's/\"//g')
-network="sphinx"
-type="validator"
-foldersize=$(du -hs $HOME/.shardeum | awk '{print $1}')
+port=$(cat ~/.shardeum/.env | grep SHMEXT | cut -d "=" -f 2)
+node_status=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.status | sed 's/"//g')
 
-echo "updated='$(date +'%y-%m-%d %H:%M')'"
-echo "version='$version'"
-echo "status=$status" 
-echo "note='$note'"
-echo "network=$network"
-echo "type=$type"
-echo "folder=$foldersize"
-echo "id=$id"
-echo "statusapi="$sts
+case $node_status in
+ null) status="ok";note="standby ($note_status)" ;;
+ active) status="ok";note="active" ;;
+ *) status="error";note="API error ($note_status)" ;;
+esac
+
+case $docker_status in
+  running) ;;
+  *) status="error"; note="docker not running" ;;
+esac
+
+cat << EOF
+{
+  "project":"$folder",
+  "id":$SHARDEUM_ID,
+  "machine":"$MACHINE",
+  "chain":"sphinx",
+  "type":"node",
+  "status":"$status",
+  "note":"$note",
+  "docker":"$docker_status",
+  "folder_size":"$folder_size",
+  "updated":"$(date --utc +%FT%TZ)",
+  "docker_status":"$docker_status",
+  "port":"$port",
+  "version":"$version",
+}
+EOF
