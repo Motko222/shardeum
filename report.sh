720f750a61c40320fdb1f54642b6e0521d686d3f
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-28 23:24:43 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -3,9 +3,12 @@
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 docker_status=$(docker inspect shardeum-dashboard | jq -r .[].State.Status)
 folder_size=$(du -hs $HOME/.shardeum | awk '{print $1}')
-port=$(cat ~/.shardeum/.env | grep SHMEXT | cut -d "=" -f 2)
-version=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.appData.shardeumVersion | sed 's/\"//g')
-node_status=$(curl -s http://localhost:$port/nodeinfo | jq .nodeInfo.status | sed 's/"//g')
+ext_port=$(cat ~/.shardeum/.env | grep SHMEXT | cut -d "=" -f 2)
+dash_port=$(cat ~/.shardeum/.env | grep DASHPORT | cut -d "=" -f 2)
+server_ip=$(cat ~/.shardeum/.env | grep SERVERIP | cut -d "=" -f 2)
+
+version=$(curl -s http://localhost:$ext_port/nodeinfo | jq .nodeInfo.appData.shardeumVersion | sed 's/\"//g')
+node_status=$(curl -s http://localhost:$ext_port/nodeinfo | jq .nodeInfo.status | sed 's/"//g')
 
 case $node_status in
  null) status="ok";note="standby ($note_status)" ;;
@@ -21,7 +24,7 @@ esac
 cat << EOF
 {
   "project":"$folder",
-  "id":$SHARDEUM_ID,
+  "id":"$SHARDEUM_ID",
   "machine":"$MACHINE",
   "chain":"sphinx",
   "type":"node",
@@ -32,6 +35,7 @@ cat << EOF
   "docker_status":"$docker_status",
   "node_status":"$node_status",
   "port":"$port",
-  "version":"$version"
+  "version":"$version",
+  "links": { "name":"dash", "url":"http://$server_ip:$dash_port/maintenance" }
 }
 EOF
