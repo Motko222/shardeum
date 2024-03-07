be1b4fc085c22d97dc576dcad793db018ad21bb3
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-07 22:53:23 +0100

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -5,6 +5,9 @@ curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
 chmod +x installer.sh
 ./installer.sh
 
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$folder/conf
+
 if [ ! -z $HOST ]
 then
  curl --request POST \
