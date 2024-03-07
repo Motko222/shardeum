4d0c59edecdb87964ab9ea095d7071c09579e88f
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-07 22:52:30 +0100

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -4,3 +4,15 @@ cd ~
 curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh
 chmod +x installer.sh
 ./installer.sh
+
+if [ ! -z $HOST ]
+then
+ curl --request POST \
+ "$HOST/api/v2/write?org=$ORG&bucket=node&precision=ns" \
+  --header "Authorization: Token $TOKEN" \
+  --header "Content-Type: text/plain; charset=utf-8" \
+  --header "Accept: application/json" \
+  --data-binary '
+    update,node='$ID',machine='$MACHINE' version="'$version'" '$(date +%s%N)' 
+    '
+fi
