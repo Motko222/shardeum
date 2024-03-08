2ee59f891761a63916303377dfb4fee4b336bd0a
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-08 17:13:09 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -55,6 +55,6 @@ then
   --header "Content-Type: text/plain; charset=utf-8" \
   --header "Accept: application/json" \
   --data-binary '
-    status,node='$id',machine='$MACHINE' status="'$status'",message="'$message'",version="'$version'" '$(date +%s%N)' 
+    status,node='$id',machine='$MACHINE' status="'$status'",message="'$message'",version="'$version'",url="'$url'" '$(date +%s%N)' 
     '
 fi
