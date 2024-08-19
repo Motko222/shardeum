8eb2e71068a949a4dd4de156b43b563a0f0a468a
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-19 21:27:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-  sudo apt update
-  apt upgrade -y
-  sudo apt-get install curl jq
-  sudo apt install docker.io
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-  curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh | bash
+sudo apt update
+apt upgrade -y
+sudo apt-get install curl jq
+sudo apt install docker.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+cd ~
+wget https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh 
+bash installer.sh
+rm installer.sh
