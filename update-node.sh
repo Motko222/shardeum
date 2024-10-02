e643c01aff3fc064d6ed598b3705f76f94fd129a
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-02 12:09:54 +0200

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh | bash
+/bin/bash -c $(curl -O https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh)
