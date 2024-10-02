f2abad460957da5704527001a9182808ab170bfd
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-02 12:12:49 +0200

    Update and rename install.sh to install-dependancies.sh

--- install-dependancies.sh
+++ install-dependancies.sh
@@ -6,9 +6,3 @@ sudo apt-get install curl jq
 sudo apt install docker.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-cd ~
-rm -r .shardeum
-wget https://raw.githubusercontent.com/shardeum/validator-dashboard/main/installer.sh -O installer.sh
-bash installer.sh
-rm installer.sh
